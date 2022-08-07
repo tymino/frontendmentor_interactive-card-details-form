4defa40d0291d527d2d31c2aef322098be92cca2
tymino <d_team-lex@bk.ru>
2022-08-07 12:31:34 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="app">Learn React</div>;
+};
+
+export default App;
---
+++ src/components/Attribution/Attribution.jsx
@@ -0,0 +1,18 @@
+import './Attribution.sass';
+
+const Attribution = () => {
+  return (
+    <div class="attribution">
+      Challenge by{' '}
+      <a
+        href="https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw"
+        target="_blank"
+        rel="noreferrer">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://github.com/tymino">tymino</a>.
+    </div>
+  );
+};
+
+export default Attribution;
